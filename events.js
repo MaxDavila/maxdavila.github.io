04fc661be06a2649c7405f1123deb06e8662eb6f
maxdavila <adavila0711@gmail.com>
2013-11-08 02:31:43 -0800

    add toggle full project view

--- events.js
+++ events.js
@@ -1,27 +1,37 @@
 $('document').ready(function(){
   bindDownArrow();
   bindWells();
+  bindFullWell();
 });
 
 function bindDownArrow() {
-  $('.fa-arrow-circle-down').on("click", scrollToPortfolio);
-  $('#portfolio-link').on("click", scrollToPortfolio);
+  $('.fa-arrow-circle-down').on("click", {selector: $('#portfolio')}, scrollToSelector);
+  $('#portfolio-link').on("click", {selector: $('#portfolio')}, scrollToSelector);
 }
 
-function scrollToPortfolio() {
+function scrollToSelector(event, selector) {
+  console.log(event)
     $('body,html').animate({
-      scrollTop: $('#portfolio').offset().top
+      scrollTop: event.data.selector.offset().top
     }, 600);
 }
 
 function bindWells() {
-$('.well').hover(function(){
-  $(this).nextAll('.well').stop().animate({ opacity: '0.5' }, 500)
-  $(this).prevAll('.well').stop().animate({ opacity: '0.5' }, 500)
+  $('.well').hover(function(){
+    $(this).nextAll('.well').stop().animate({ opacity: '0.5' }, 500)
+    $(this).prevAll('.well').stop().animate({ opacity: '0.5' }, 500)
 
-}, function(){
-  $(this).nextAll('.well').stop().animate({ opacity: '1' }, 500)
-  $(this).prevAll('.well').stop().animate({ opacity: '1' }, 500)
-
-})
+  }, function(){
+    $(this).nextAll('.well').stop().animate({ opacity: '1' }, 500)
+    $(this).prevAll('.well').stop().animate({ opacity: '1' }, 500)
+  })
 }
+
+function bindFullWell(){
+    $('.well.moodlist').on('click', function(){
+    $sibling = $(this).nextAll('.moodlist')
+    $sibling.toggle()
+
+  })
+}
+
