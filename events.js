081092c8a3f51d63af158189aa9fb7f36e859b4b
maxdavila <adavila0711@gmail.com>
2013-11-08 00:04:26 -0800

    add well hover

--- events.js
+++ events.js
@@ -1,11 +1,27 @@
 $('document').ready(function(){
   bindDownArrow();
+  bindWells();
 });
 
 function bindDownArrow() {
-  $('.fa-arrow-circle-down').on("click", function(){
+  $('.fa-arrow-circle-down').on("click", scrollToPortfolio);
+  $('#portfolio-link').on("click", scrollToPortfolio);
+}
+
+function scrollToPortfolio() {
     $('body,html').animate({
       scrollTop: $('#portfolio').offset().top
     }, 600);
-    });
-};
+}
+
+function bindWells() {
+$('.well').hover(function(){
+  $(this).nextAll('.well').stop().animate({ opacity: '0.5' }, 500)
+  $(this).prevAll('.well').stop().animate({ opacity: '0.5' }, 500)
+
+}, function(){
+  $(this).nextAll('.well').stop().animate({ opacity: '1' }, 500)
+  $(this).prevAll('.well').stop().animate({ opacity: '1' }, 500)
+
+})
+}
